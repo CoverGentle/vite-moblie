3b60541479401d7cee804acdfd3312634f3e3a56
tangshuaibo <tangshuaibo@ghzq.com>
2022-09-28 16:46:00 +0800

    安装path路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'src')
+    }
+  },
   plugins: [vue()],
   server: {
     open: true, //vite项目启动时自动打开浏览器
